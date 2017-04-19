b25306013e1f5cdac68ddd885cf79b569eb28c2a
Stas Pavlov <stanisp@microsoft.com>
2017-04-19 20:55:04 +0300

    Add comment to scripts

--- createdeis.sh
+++ createdeis.sh
@@ -1,19 +1,30 @@
-export AZURE_RG_NAME=stas-k8s-rg
-export AZURE_DC_LOCATION=southcentralus
-export AZURE_SERVICE_NAME=stas-k8s-acs
-export AZURE_DNS_PREFIX=stas-k8s
-export AZURE_SA_NAME=stask8sdeisstorage
+# Azure Resource Group name
+export AZURE_RG_NAME=stas-k8s-rg			 
+# Azure DC Location
+export AZURE_DC_LOCATION=southcentralus		
+# Azure Container Service name
+export AZURE_SERVICE_NAME=stas-k8s-acs		
+# Azure DNS Prefix for Azure Container Service
+export AZURE_DNS_PREFIX=stas-k8s			
+# Azure Storage Account name for DEIS usage
+export AZURE_SA_NAME=stask8sdeisstorage		
 
+# Init Helm localy and install Triller at Kubernetes cluster
 helm init
 
+# Add Deis repository to helm
 helm repo add deis https://charts.deis.com/workflow
 
+# Create Azure Storage Account
 az storage account create -n $AZURE_SA_NAME -l $AZURE_DC_LOCATION -g $AZURE_RG_NAME --sku Standard_LRS
 
+# Take Azure Account Key to pass it to helm install to use by Deis
 export AZURE_SA_KEY=`az storage account keys list -n $AZURE_SA_NAME -g $AZURE_RG_NAME --query [0].value --output tsv`
 
+# Install Deis in Kubernetes cluster to deis namespace
 helm install deis/workflow --namespace=deis --set global.storage=azure,azure.accountname=$AZURE_SA_NAME,azure.accountkey=$AZURE_SA_KEY,azure.registry_container=registry,azure.database_container=database,azure.builder_container=builder
 
+# Check that Desi pods starting at Kubernetes cluster
 kubectl --namespace=deis get pods
 
 #kubectl --namespace=deis get service deis-router
--- createk8s.sh
+++ createk8s.sh
@@ -1,17 +1,28 @@
-export AZURE_RG_NAME=stas-k8s-rg
-export AZURE_DC_LOCATION=southcentralus
-export AZURE_SERVICE_NAME=stas-k8s-acs
-export AZURE_DNS_PREFIX=stas-k8s
-export AZURE_SA_NAME=stask8sdeisstorage
+# Azure Resource Group name
+export AZURE_RG_NAME=stas-k8s-rg			 
+# Azure DC Location
+export AZURE_DC_LOCATION=southcentralus		
+# Azure Container Service name
+export AZURE_SERVICE_NAME=stas-k8s-acs		
+# Azure DNS Prefix for Azure Container Service
+export AZURE_DNS_PREFIX=stas-k8s			
+# Azure Storage Account name for DEIS usage
+export AZURE_SA_NAME=stask8sdeisstorage		
 
+# Create Azure Resource Group
 az group create --name "${AZURE_RG_NAME}" --location "${AZURE_DC_LOCATION}"
 
+# Create Azure Container Service with Kubernetes as orchestrator
+# 1 master and 1 agent
+# use SSH pub key from ~/.ssh/
 az acs create --resource-group="${AZURE_RG_NAME}" --location="${AZURE_DC_LOCATION}" \
   --orchestrator-type=kubernetes --master-count=1 --agent-count=1 \
   --agent-vm-size="Standard_D2_v2" \
   --admin-username="k8sadmin" \
   --name="${AZURE_SERVICE_NAME}" --dns-prefix="${AZURE_DNS_PREFIX}"
 
+# Downoad .kube configuration from created Kubernetes cluster to work with kubectl 
 az acs kubernetes get-credentials --resource-group=$AZURE_RG_NAME --name=$AZURE_SERVICE_NAME
 
+# Check that the cluster created and run
 kubectl cluster-info
